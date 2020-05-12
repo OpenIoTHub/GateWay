a5169c7277ffce8bfa95389d55372a8d7133544b
Farry <newfarry@126.com>
2020-05-12 16:12:36 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,28 @@
 
 import 'package:flutter/material.dart';
 import 'package:modules/pages/gateWay/findmDNSClientList.dart';
+import 'package:modules/pages/mdnsService/mDNSService/getway.dart';
+import 'package:modules/model/portService.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  var gatewayPortService = PortService(
+      portConfig: null,
+      isLocal: true,
+      info: {
+        "name": "网关",
+        "model": Gateway.modelName,
+        "mac": "mac",
+        "id": "id",
+        "author": "Farry",
+        "email": "newfarry@126.com",
+        "home-page": "https://github.com/OpenIoTHub",
+        "firmware-respository": "https://github.com/OpenIoTHub/gateway-go",
+        "firmware-version": "version",
+      },
+      ip: "localhost",
+      port: 1082);
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -13,7 +31,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.purple,
       ),
-      home: FindmDNSClientListPage(),
+      home: Gateway(serviceInfo: gatewayPortService),
       darkTheme: new ThemeData(
         primaryColor: Colors.black,
       ),
