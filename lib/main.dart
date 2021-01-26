4b61aad9ada64154bccc4e70456f5d202652a425
Yu Fang <yu@iotserv.com>
2021-01-26 14:09:14 +0800

    适应新的PortService

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 // Step 7 (Final): Change the app's theme
 import 'package:flutter/material.dart';
 import 'package:openiothub_plugin/plugins/mdnsService/mDNSService/getway.dart';
-import 'package:openiothub_models/models/portService.dart';
+import 'package:openiothub_grpc_api/pb/service.pb.dart';
+import 'package:openiothub_grpc_api/pb/service.pbgrpc.dart';
 
 void main() => runApp(new MyApp());
 //TODO 开机自启，配置保存，斐讯r1安装网关脚本
 class MyApp extends StatelessWidget {
-  var gatewayPortService = PortService(
-      isLocal: true,
-      info: {
+  static PortService get gatewayPortService {
+    PortService gatewayPortService = PortService.create();
+    gatewayPortService.isLocal = true;
+    gatewayPortService.ip = "127.0.0.1";
+    gatewayPortService.port = 1082;
+    gatewayPortService.info.addAll({
         "name": "网关",
         "model": Gateway.modelName,
         "mac": "mac",
@@ -18,9 +22,11 @@ class MyApp extends StatelessWidget {
         "home-page": "https://github.com/OpenIoTHub",
         "firmware-respository": "https://github.com/OpenIoTHub/gateway-go",
         "firmware-version": "version",
-      },
-      ip: "127.0.0.1",
-      port: 1082);
+      }
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
